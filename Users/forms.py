82114286ce0e95bb614b2596077053842ee739d0
K-Avinash-Nayak <avinashnayak2002@gmail.com>
2020-12-10 16:27:09 +0530

    Made the sign up forms functional.

---
+++ Users/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder':('Username')})
+        self.fields['email'].widget.attrs.update({'placeholder':('Email')})
+        self.fields['password1'].widget.attrs.update({'placeholder':('Password')})        
+        self.fields['password2'].widget.attrs.update({'placeholder':('Confirm password')})
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
--- Users/views.py
+++ Users/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def login(request):
-    return render(request, 'login_form.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}!')
+            return redirect('Users-home')
+        else:
+            print(form.cleaned_data)
+    else:    
+        form = SignUpForm()
+    return render(request, 'login_form.html', {'form':form})
