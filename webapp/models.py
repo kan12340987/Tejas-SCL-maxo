782cdc6e84c2b24fe6a3335ff7d108d8f2e07784
K-Avinash-Nayak <avinashnayak2002@gmail.com>
2021-01-03 11:55:34 +0530

    Version 2 of backend

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User as User_model
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+
+class Texbook(models.Model):
+    Title = models.CharField(default = '', max_length=100)
+    Author = models.CharField(default = '', max_length=50)
+    Branch = models.CharField(default = '', max_length=50)
+    Subject = models.CharField(default='', max_length=50)
+    PublicationYear = models.IntegerField(default = 2021)
+    link = models.URLField(default='')
+
+class QPapers(models.Model):
+    Branch = models.CharField(default = '', max_length=50)
+    Subject = models.CharField(default='', max_length=50)
+    year = models.IntegerField(default = 2021)
+    Semester = models.IntegerField(default= 1)
+    college = models.CharField(default = '', max_length=100)
+    link = models.URLField(default='')
+
+class notes(models.Model):
+    Branch = models.CharField(default = '', max_length=50)
+    Subject = models.CharField(default='', max_length=50)
+    year = models.IntegerField(default = 2021)
+    Semester = models.IntegerField(default= 1)
+    college = models.CharField(default = '', max_length=50)   
+    link = models.URLField(default='')
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from webapp.models import QPapers, notes, Texbook
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'index.html')
@@ -8,8 +10,23 @@ def home(request):
 def about(request):
     return render(request, 'About_us.html')
 
+@login_required
 def notebook(request):
-    return render(request, 'Notebook.html')
+    context = {
+        'notes': notes.objects.all()
+    }
+    return render(request, 'NotebookSecondary.html',context)
+
+@login_required
+def questionpapers(request):
+    context = {
+        'qpapers': QPapers.objects.all()
+    }
+    return render(request, 'QuestionPaperSecondary.html', context)
+
+@login_required
+def textbook(request):
+    return render(request, 'TextbookSecondary.html')
 
 @login_required
 def landingpage(request):
