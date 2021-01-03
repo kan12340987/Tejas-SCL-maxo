6f2012bafe625bcf579cbc754b29c08602a7650a
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-01-03 17:41:50 +0530

    Made minor changes to webapp

--- webapp/models.py
+++ webapp/models.py
@@ -7,25 +7,25 @@ from django.core.validators import RegexValidator
 alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
 
 class Texbook(models.Model):
-    Title = models.CharField(default = '', max_length=100)
-    Author = models.CharField(default = '', max_length=50)
-    Branch = models.CharField(default = '', max_length=50)
-    Subject = models.CharField(default='', max_length=50)
+    Title = models.TextField(default = '', max_length=100)
+    Author = models.TextField(default = '', max_length=100)
+    Branch = models.TextField(default = '', max_length=100)
+    Subject = models.TextField(default='', max_length=100)
     PublicationYear = models.IntegerField(default = 2021)
     link = models.URLField(default='')
 
 class QPapers(models.Model):
-    Branch = models.CharField(default = '', max_length=50)
-    Subject = models.CharField(default='', max_length=50)
+    Branch = models.TextField(default = '', max_length=100)
+    Subject = models.TextField(default='', max_length=100)
     year = models.IntegerField(default = 2021)
     Semester = models.IntegerField(default= 1)
-    college = models.CharField(default = '', max_length=100)
+    college = models.TextField(default = '', max_length=100)
     link = models.URLField(default='')
 
 class notes(models.Model):
-    Branch = models.CharField(default = '', max_length=50)
-    Subject = models.CharField(default='', max_length=50)
+    Branch = models.TextField(default = '', max_length=100)
+    Subject = models.TextField(default='', max_length=100)
     year = models.IntegerField(default = 2021)
     Semester = models.IntegerField(default= 1)
-    college = models.CharField(default = '', max_length=50)   
+    college = models.TextField(default = '', max_length=100)   
     link = models.URLField(default='')
