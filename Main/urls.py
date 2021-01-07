9f9e1441986d425fb3a4e1c82887ab5b5b1e9541
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-01-07 16:47:31 +0530

    deev

--- Main/urls.py
+++ Main/urls.py
@@ -19,7 +19,7 @@ from Users import views as users_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+#heroku
 from django.views.static import serve
 from django.conf.urls import url
 
@@ -33,7 +33,7 @@ urlpatterns = [
     path('oauth/', include('social_django.urls', namespace='social')), #path of social login
     #heroku
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
     
 ]
