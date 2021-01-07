60c4e54c67f0306bf52c67819a804be6876891c1
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-01-07 17:02:16 +0530

    eve

--- Main/urls.py
+++ Main/urls.py
@@ -35,7 +35,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-    
-]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
