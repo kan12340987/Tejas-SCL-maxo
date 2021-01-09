99817d138d3aaf6ac8a9f9af7f9cc22f6a3979a9
K-Avinash-Nayak <avinashnayak2002@gmail.com>
2021-01-09 20:57:30 +0530

    Cleaned the repo. Made the profile functional, and fixed the issues with login and signup.

--- Users/forms.py
+++ Users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Profile
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
@@ -14,3 +15,21 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+    
+class UserUpdateFrom(forms.ModelForm):
+     email = forms.EmailField()
+
+     class Meta:
+        model = User
+        fields = ['username','email']            
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+
+
+class NameForm(forms.ModelForm):
+    class Meta:
+        model= User
+        fields = ['first_name', 'last_name']
