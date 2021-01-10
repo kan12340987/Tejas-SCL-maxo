fb0f89b817e9d2232608290f4574d3141f294cab
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-01-10 22:28:36 +0530

    Merge branch 'forum' into main

--- Main/settings.py
+++ Main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u+1nty8^qky#f^*(1mlb3*&yexub%97&x1h=337%wn-&p_ga7o'
+SECRET_KEY = "u+1nty8^qky#f^*(1mlb3*&yexub%97&x1h=337%wn-&p_ga7o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
